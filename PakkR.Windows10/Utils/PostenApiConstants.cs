f502eae36945aa2ca92c2eade284614750407421
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-02-28 15:06:15 +0100

    Adds constants for API endpoints

--- PakkR.Windows10/Utils/PostenApiConstants.cs
+++ PakkR.Windows10/Utils/PostenApiConstants.cs
@@ -10,5 +10,9 @@ namespace PakkR.Windows10.Utils
     {
         public const string LoginStartUrl = "https://sporing.posten.no/receive.html";
         public const string LoginActualLoginUrl = "https://id.posten.no/login";
+
+        public const string UserProfileApiUrl = "https://sporing.posten.no/api/receive/web/protected/user.json";
+        public const string MyPackagesApiUrl = "https://sporing.posten.no/api/receive/web/protected/mykollis.json";
+        public const string SinglePackageApiUrl = "https://sporing.posten.no/api/receive/web/protected/kolli/";
     }
 }
