2294d8e54d4eeeda8ffaf7c4439659e86f2bd469
Henning Mosand Stephansen <henningmosand@outlook.com>
2016-02-28 15:42:30 +0100

    Removes unused usings

--- PakkR.Windows10/Utils/PostenApiConstants.cs
+++ PakkR.Windows10/Utils/PostenApiConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PakkR.Windows10.Utils
+﻿namespace PakkR.Windows10.Utils
 {
     public class PostenApiConstants
     {
